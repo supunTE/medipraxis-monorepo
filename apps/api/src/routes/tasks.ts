8860774372cd4d817e07f40df8ff86a17dcee8bd
rashidsajahan <rashidsajahan@gmail.com>
2025-12-24 19:17:57 +0530

    feat: integrate zod validation for task routes

--- apps/api/src/routes/tasks.ts
+++ apps/api/src/routes/tasks.ts
@@ -1,12 +1,14 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { TaskController } from "../controllers";
+import { createTaskSchema , taskDetailsSchema, updateTaskSchema } from "../../../../packages/models/tasks/task.schema";
 import type { Env } from "../types";
 
 const tasks = new Hono<{ Bindings: Env }>();
 
 tasks.get("/", TaskController.getAllTasks);
-tasks.get("/:id", TaskController.getTaskById);
-tasks.post("/", TaskController.createTask);
-tasks.put("/:id", TaskController.updateTask);
+tasks.get("/:id", zValidator("json", taskDetailsSchema), TaskController.getTaskById);
+tasks.post("/", zValidator("json", createTaskSchema), TaskController.createTask);
+tasks.put("/:id", zValidator("json", updateTaskSchema), TaskController.updateTask);
 
 export default tasks;
