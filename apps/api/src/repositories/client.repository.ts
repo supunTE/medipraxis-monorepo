e87d4cae82929518878bfd1165c7cabeaf5b0960
rashidsajahan <rashidsajahan@gmail.com>
2026-01-10 18:51:45 +0530

    feat: add contact info methods for retrieving and creating contact information, contatc id used to create client

--- apps/api/src/repositories/client.repository.ts
+++ apps/api/src/repositories/client.repository.ts
@@ -1,6 +1,8 @@
 import type {
   Client,
+  ContactInfo,
   CreateClientInput,
+  CreateContactInfoInput,
   UpdateClientInput,
 } from "@repo/models";
 import type { SupabaseClient } from "@supabase/supabase-js";
@@ -16,6 +18,47 @@ export class ClientRepository {
   constructor(db: SupabaseClient) {
     this.db = db;
   }
+
+  /* ------------Contact Info Methods------------ */
+
+  async findContactInfo(
+    countryCode: string,
+    contactNumber: string
+  ): Promise<ContactInfo | null> {
+    const { data, error } = await this.db
+      .from("contact_info")
+      .select("*")
+      .eq("country_code", countryCode)
+      .eq("contact_number", contactNumber)
+      .single();
+
+    if (error || !data) {
+      return null;
+    }
+
+    return data as ContactInfo;
+  }
+
+  async createContactInfo(
+    contactInfo: CreateContactInfoInput
+  ): Promise<ContactInfo> {
+    const data = {
+      country_code: contactInfo.country_code,
+      contact_number: contactInfo.contact_number,
+    };
+    const { data: contact, error } = await this.db
+      .from("contact_info")
+      .insert(data)
+      .select()
+      .single();
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    return contact as ContactInfo;
+  }
+
+  /* ------------Client Methods------------ */
 
   async findAll(userId?: string): Promise<Client[]> {
     let query = this.db
@@ -67,6 +110,7 @@ export class ClientRepository {
         clientData.emergency_contact_relationship || null,
       known_conditions: clientData.known_conditions || null,
       note: clientData.note || null,
+      contact_id: clientData.contact_id,
       user_id: clientData.user_id,
       modified_date: new Date().toISOString(),
     };
