b2396c433c2d679a059ef70f3abbca6384836d65
Speedy <bimalshahasanthi@gmail.com>
2026-03-08 15:18:35 +0530

    fix: remove string value and add json for form table

--- packages/models/src/form/form.schema.ts
+++ packages/models/src/form/form.schema.ts
@@ -10,18 +10,29 @@ export interface Form {
   description: string | null;
   version: number;
   is_active: boolean;
-  form_configuration: string; // JSON string
+  form_configuration: Record<string, unknown>[]; // JSONB array
   user_id: string;
   form_type: string;
   created_date: string;
   updated_date: string;
 }
 
+export const formFieldSchema = z.object({
+  id: z.string().uuid(),
+  active: z.boolean(),
+  sequence: z.number(),
+  help_text: z.string(),
+  field_type: z.string(),
+  description: z.string(),
+  display_label: z.string(),
+  options: z.array(z.string()).optional(),
+});
+
 export const createFormSchema = z.object({
   title: z.string().min(1),
   description: z.string().optional().nullable(),
   is_active: z.boolean().optional().default(true),
-  form_configuration: z.string(), // JSON string
+  form_configuration: z.array(formFieldSchema),
   user_id: z.string().uuid(),
   form_type: z.nativeEnum(FormType),
 });
